7b0f29ecdf08f3fa8c60c09bd54976a80ad9691d
Kiselyaka <kiselyaka12s@gmail.com>
2026-04-21 23:43:23 +0300

    сломалось

--- backend/src/routes/analytics.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const analyticsController = require('../controllers/analyticsController');
-const { auth } = require('../middleware/auth');
-
-router.get('/productivity', auth, analyticsController.getProductivity);
-router.get('/export', auth, analyticsController.exportData);
-
-module.exports = router;
